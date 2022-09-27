d2b37db44a444f2246afd5a84f5357e23fe95d1b
FaizulOsman <faizulosman@gmail.com>
2022-09-27 12:14:21 +0100

    cart count debug solution done

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -4,21 +4,27 @@ import React from 'react';
 import './Cart.css'
 
 const Cart = ({ cart }) => {
+    let quantity = 0;
+    let total = 0;
+    let shipping = 0;
 
-    // cart.map(product => console.log(product.price))
-    const total = cart.reduce((x, y) => x + y.price, 0)
-    const totalShippingCharge = cart.reduce((x, y) => x + y.shipping, 0)
+    for (const product of cart) {
+        quantity = quantity + product.quantity;
+        total = total + product.price * product.quantity;
+        shipping = shipping + product.shipping;
+    }
+
+
     const tax = total / 10
-    const grandTotal = total + totalShippingCharge + tax
-    console.log(cart);
+    const grandTotal = total + shipping + tax
 
     return (
         <div className='cart'>
             <h3 className='heading'>Order Summary</h3>
             <div className='cart-details'>
-                <p>Selected Items: {cart.length}</p>
+                <p>Selected Items: {quantity}</p>
                 <p>Total Price: $ {total}</p>
-                <p>Total Shipping Charge: ${totalShippingCharge}</p>
+                <p>Total Shipping Charge: ${shipping}</p>
                 <p>Tax: ${tax.toFixed(2)}</p>
                 <h3>Grand Total: ${grandTotal.toFixed(2)}</h3>
             </div>
